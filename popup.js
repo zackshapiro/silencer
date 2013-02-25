9f4c67fcec524351f5f0aaa275a198384dc961e9
Zack Shapiro <zack@59thirty.com>
2013-02-24 18:01:07 -0800

    first commit

---
+++ popup.js
@@ -0,0 +1,13 @@
+filterTerms = [
+  "oscars",
+  "vine.co",
+  "4sq.com",
+  "shooting",
+  "china",
+  "techcrunch",
+  "tcrn.ch"
+]
+
+  console.log("Welcome to Silencer");
+  
+  alert(filterTerms);
