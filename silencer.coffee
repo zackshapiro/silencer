74e5c203a3137137c2a35d70327b57f9adda7cce
Zack Shapiro <zack@59thirty.com>
2013-03-06 23:42:30 -0800

    muting twitterga. gp gp

--- silencer.coffee
+++ silencer.coffee
@@ -1,21 +1,47 @@
 $ ->
 
+  # TO DO: Convert search terms to lowercase
   filterTerms = [
     "Apple",
     "Frank Chimero",
     "Coke",
     "Privacy",
     "San Francisco",
-    "Canon"
+    "Canon",
+    "4sq.com",
+    "vine.co",
+    "Andrew Hyde",
+    "#sxsw",
+    "#sxsw2013",
+    "instagr.am"
   ]
 
-  chrome.extension.sendMessage({greeting: "hello"}, (response) -> console.log(response.farewell))
+  # chrome.extension.sendMessage({greeting: "hello"}, (response) -> console.log(response.farewell))
+
+  hideChild = (child) ->
+    child.hide()
 
   if $('#queue').length > 0
     articles = $('#queue')
     articlesArray = articles.children()
-    articlesLength = articles.children().length
+    articlesLength = articlesArray.length
 
     for article in articlesArray
       for term in filterTerms
-        if $($(article).find(".title").children()[0]).text().indexOf(term) > -1 then $(article).hide()
+        if $($(article).find(".title").children()[0]).text().indexOf(term) > -1 then hideChild($(article))
+
+
+  if $(".stream-items").children().length > 0
+    tweets = $(".stream-items")
+    tweetsArray = tweets.children()
+    tweetsLength = tweetsArray.length
+
+    for tweet in tweetsArray
+      for term in filterTerms
+        # if $($(tweet).find(".js-tweet-text")).text().indexOf(term) > -1 then hideChild($(tweet))
+        # if $($(tweet).find(".fullname")).text().indexOf(term) > -1 then hideChild($(tweet))
+        if $($(tweet)).text().indexOf(term) > -1 then hideChild($(tweet))
+        if $($(tweet).find("js-index-of")).text().indexOf(term) > -1 then hideChild($(tweet))
+
+        # I can do this too $(".stream-items").children().first().text().indexOf("theeconomist")
+        # searches the entire tweet, don't have to specify class names or ids
--- silencer.js
+++ silencer.js
@@ -2,27 +2,42 @@
 (function() {
 
   $(function() {
-    var article, articles, articlesArray, articlesLength, filterTerms, term, _i, _len, _results;
-    filterTerms = ["Apple", "Frank Chimero", "Coke", "Privacy", "San Francisco", "Canon"];
-    chrome.extension.sendMessage({
-      greeting: "hello"
-    }, function(response) {
-      return console.log(response.farewell);
-    });
+    var article, articles, articlesArray, articlesLength, filterTerms, hideChild, term, tweet, tweets, tweetsArray, tweetsLength, _i, _j, _k, _len, _len1, _len2, _results;
+    filterTerms = ["Apple", "Frank Chimero", "Coke", "Privacy", "San Francisco", "Canon", "4sq.com", "vine.co", "Andrew Hyde", "#sxsw", "#sxsw2013", "instagr.am"];
+    hideChild = function(child) {
+      return child.hide();
+    };
     if ($('#queue').length > 0) {
       articles = $('#queue');
       articlesArray = articles.children();
-      articlesLength = articles.children().length;
-      _results = [];
+      articlesLength = articlesArray.length;
       for (_i = 0, _len = articlesArray.length; _i < _len; _i++) {
         article = articlesArray[_i];
+        for (_j = 0, _len1 = filterTerms.length; _j < _len1; _j++) {
+          term = filterTerms[_j];
+          if ($($(article).find(".title").children()[0]).text().indexOf(term) > -1) {
+            hideChild($(article));
+          }
+        }
+      }
+    }
+    if ($(".stream-items").children().length > 0) {
+      tweets = $(".stream-items");
+      tweetsArray = tweets.children();
+      tweetsLength = tweetsArray.length;
+      _results = [];
+      for (_k = 0, _len2 = tweetsArray.length; _k < _len2; _k++) {
+        tweet = tweetsArray[_k];
         _results.push((function() {
-          var _j, _len1, _results1;
+          var _l, _len3, _results1;
           _results1 = [];
-          for (_j = 0, _len1 = filterTerms.length; _j < _len1; _j++) {
-            term = filterTerms[_j];
-            if ($($(article).find(".title").children()[0]).text().indexOf(term) > -1) {
-              _results1.push($(article).hide());
+          for (_l = 0, _len3 = filterTerms.length; _l < _len3; _l++) {
+            term = filterTerms[_l];
+            if ($($(tweet)).text().indexOf(term) > -1) {
+              hideChild($(tweet));
+            }
+            if ($($(tweet).find("js-index-of")).text().indexOf(term) > -1) {
+              _results1.push(hideChild($(tweet)));
             } else {
               _results1.push(void 0);
             }
